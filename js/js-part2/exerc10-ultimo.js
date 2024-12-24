a4e5da8be6b98d18c896b85d97d4586ac192397d
Moises Silva <moiss.silva@outlook.com>
2024-12-24 17:57:15 -0300

    Criação das funções novaVaga e listarVaga do exercício 10.

--- js/js-part2/exerc10-ultimo.js
+++ js/js-part2/exerc10-ultimo.js
@@ -30,47 +30,83 @@ usuário confirme a exclusão da vaga antes de realmente exclui-la.
 
 */
 
+let vagas = []
+
 // Functions
-let vagas = {}
-let candidatos = {}
+function novaVaga() {
+  const nome = prompt("Informe um nome para a vaga:")
+  const descricao = prompt("Informe um descrição para a vaga:")
+  const dataLimite = prompt("Informe um data limite (dd/mm/aaaa) para a vaga:")
 
+  const confirmacao = confirm("Deseja criar uma nova vaga com essas informações?\n" +
+    "Nome: " + nome +
+    "\nDescrição: " + descricao +
+    "\nData limite: " + dataLimite
+  )
 
-// **********************************************
-const texto = "O que deseja fazer?\n\n"+
-"1 - Listar vagas disponíveis\n" +
-"2 - Criar um nova vaga\n"+
-"3 - Visualizar uma vaga\n"+
-"4 - Inscrever um candidato em uma vaga\n"+
-"5 - Excluir uma vaga\n"+
-"0 - Sair"
-
-let opt = ""
-do {
-  opt = prompt(texto)
-  
-  switch (opt) {
-    case "1":
-      
-      break;
-    case "2":
-      
-      break;
-    case "3":
-      
-      break;
-    case "4":
-      
-      break;
-    case "5":
-      
-      break;
-    case "0":
-      alert("Saindo...")
-      break;
-  
-    default:
-      alert("Opção Inválida")
-      break;
+  if (confirmacao) {
+    // até esta linha o array vagas está vazio e sem formato de dados
+    const novaVaga = { nome, descricao, dataLimite, candidatos: [] }
+    // agora o array vagas possui as strings nome, descr e dataLimite, e um array vazio chamado candidatos 
+    vagas.push(novaVaga)
+    alert("Vaga criada.")
   }
+}
 
-} while (opt !== "0");
+function listarVagas() {
+    const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => { // o parametro vaga busca em vagas (que esta chamando reduce)
+    textoFinal += indice + ". "
+    textoFinal += vaga.nome
+      textoFinal += " (" + vaga.candidatos.length + " candidatos)\n" // na função novaVaga a estrutura de dador foi criada com um array vazio, Candidatos.
+      // quando eu criar um candidato ele será colocado nesse array que, ao consultar a vaga, aparecerá a quantidade de candidato aqui.
+      return textoFinal // este texto final é a concatenação dos dados do array vaga, que foi formado pelo recude, vindo de vagas
+      
+  }, "")
+
+  alert(vagasEmTexto)
+}
+
+function myMain() {
+  // **********************************************
+  const texto = "O que deseja fazer?\n\n" +
+    "1 - Listar vagas disponíveis\n" +
+    "2 - Criar um nova vaga\n" +
+    "3 - Visualizar uma vaga\n" +
+    "4 - Inscrever um candidato em uma vaga\n" +
+    "5 - Excluir uma vaga\n" +
+    "0 - Sair"
+
+  let opt = ""
+  do {
+    opt = prompt(texto)
+  
+    switch (opt) {
+      case "1":
+        listarVagas()
+        break;
+      case "2":
+        novaVaga()
+        break;
+      case "3":
+      
+        break;
+      case "4":
+      
+        break;
+      case "5":
+      
+        break;
+      case "0":
+        alert("Saindo...")
+        break;
+  
+      default:
+        alert("Opção Inválida")
+        break;
+    }
+
+  } while (opt !== "0")
+
+}
+
+myMain()
